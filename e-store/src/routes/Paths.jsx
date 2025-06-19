cd68fc65c4c6ba2edbdb0d9c7e6098297f9f60cb
Diogoalves-dev <diogoribeirodev1@gmail>
2025-06-19 11:28:07 -0300

    ajustando o codigo de rotas

--- e-store/src/routes/Paths.jsx
+++ e-store/src/routes/Paths.jsx
@@ -13,10 +13,8 @@ const Paths = () =>{
                 <Routes>
                     <Route path="/" element={<Layout/>}>
                         <Route path="/" element={<Home/>}/>
-                        <Route path="/productlist" element={<ProductList/>}/>
                         <Route path="/productadd" element={<ProductAdd/>}/>
                         <Route path="/productedit" element={<ProductEdit/>}/>
-                        <Route path="/productremove" element={<ProductRemove/>}/>
                     </Route>
                 </Routes>
             </BrowserRouter>
