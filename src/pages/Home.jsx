589317d0914d44ac097c8136d3de0174baa57f73
Diogoalves-dev <diogoribeirodev1@gmail>
2025-07-01 17:18:07 -0300

    feat: criando o layout da pagina

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,56 @@
+import styled from "styled-components";
+
+const DashboardComponent = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding-right: 20px;
+    gap: 25px;
+
+    & #containerTop{
+        display: flex;
+        justify-content: space-between;
+        
+        & .dados{
+            background-color: #1F2937;
+            width: 340px;
+            height: 65px;
+            border-radius: 10px;
+        }
+    }
+
+    & #containerBottom{
+        background-color: #1F2937;
+        height: 65vh;
+        border-radius: 10px;
+        
+    }
+
+`;
+
 const Home = () =>{
 
     return(
-        <>
-            <div id="outlet">
-                <h1>espaço destinado a homepage</h1>
-            </div>
-            
-        </>
+        <DashboardComponent id="outlet">
+                <h1>Dashboard</h1>
+
+                <div id="containerTop">
+                    <div className="dados">
+                        
+                    </div>
+                    
+                    <div className="dados">
+
+                    </div>
+                    
+                    <div className="dados">
+
+                    </div>
+                </div>
+
+                <div id="containerBottom">
+
+                </div>
+        </DashboardComponent>
     );
 };
 
