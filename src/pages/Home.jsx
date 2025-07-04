933686dbf78a7976f174d5c33b5af65fdcb4e26b
Diogoalves-dev <diogoribeirodev1@gmail>
2025-07-04 02:19:51 -0300

    buscando alguns produtos e inserindo no dashboard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,8 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import produtosImage from "../assets/dashboardImages/produtos.png";
 import vendasImage from "../assets/dashboardImages/vendas.png";
-import estoqueImage from "../assets/dashboardImages/estoque.png"
+import estoqueImage from "../assets/dashboardImages/estoque.png";
+import { useEffect, useState } from "react";
 
 const DashboardComponent = styled.div`
     display: flex;
@@ -36,18 +37,21 @@ const DashboardComponent = styled.div`
 
     & #containerBottom{
         background-color: #1F2937;
-        height: 65vh;
+        height: auto;
+        display: flex;
+        flex-direction: column;
+        gap: 30px;
         border-radius: 10px;
         
         display: flex;
         flex-direction: column;
         justify-content: space-between;
         
-        & div{
-            display: flex;
-            justify-content: space-between;
+        & #cabecalho{
             padding: 30px;
             padding-bottom: 0;
+            display: flex;
+            justify-content: space-between;          
             height: 10%;
 
             & #addProduto{
@@ -70,19 +74,111 @@ const DashboardComponent = styled.div`
         }
 
         & #produtos{
-            height: 70%;
+            display: grid;
+            grid-template-columns: 260px 260px 260px 260px;
+            flex-wrap: wrap;
+            height: 300px;
+            align-items: center;
+            justify-content: space-around;
             border-top: 1px solid #c7c7c7;
             border-bottom: 1px solid #c7c7c7;
+
+            & .produto{
+                height: 250px;
+                width: 260px;
+                border-radius: 10px;
+                border:1px solid #c7c7c7;
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+                padding-bottom: 10px;
+                gap: 10px;
+                transition: 200ms;
+
+                & img{
+                    width: 100%;
+                    height: 60%;
+                    border-top-left-radius: 10px;
+                    border-top-right-radius: 10px;
+                }
+
+                & h1{
+                    padding-left: 5px;
+                    transition: 200ms;
+                    font-size: 100%;
+                }
+
+                & p{
+                    padding-left: 5px;
+                    transition: 200ms;
+                    font-size: 100%;
+                    color: #c7c7c7;
+                }
+
+                & .botoes{
+                    padding: 0 5px;
+                    display: flex;
+                    justify-content: space-between;
+                    
+                    & .editar{
+                        background-color: #39414C;
+                        width: 65%;
+                        height: 30px;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        border-radius: 10px;
+                        color: white;
+                        transition: 200ms;
+                    }
+
+                    & .editar:hover{
+                        background-color: #4a525e;
+                        transition: 200ms;
+                    }
+
+                    & .remover{
+                        background-color: #973e3e;
+                        width: 30%;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        color: white;
+                        border-radius: 10px;
+                        transition: 200ms;
+                    }
+
+                    & .remover:hover{
+                        background-color: #be5252;
+                        transition: 200ms;
+                    }
+                }
+            }
+
+            & .produto:hover{
+                cursor: pointer;
+                transition: 200ms;
+                height: 255px;
+                width: 265px;
+
+                & h1{
+                    transition: 200ms;
+                    font-size: 110%;
+                }
+
+                & p{
+                    transition: 200ms;
+                    font-size: 110%;
+                }
+            }
         }
 
         & #pageProdutoContainer{
-            all: unset;
-
-            height: 14%;
-            padding-right: 30px;
             display: flex;
-            justify-content: end;
             align-items: center;
+            justify-content: end;
+            padding: 30px;
+            padding-top: 0px;
             
             & #pageProdutos{
                 all: unset;
@@ -108,6 +204,19 @@ const DashboardComponent = styled.div`
 `;
 
 const Home = () =>{
+    const [produtos, setProdutos] = useState([]);
+    const [count, setCount] = useState(0);
+    
+    async function buscarDados() {
+        const require = await fetch(`https://6866a33789803950dbb37048.mockapi.io/apiv1/produtos`);
+        const dados = await require.json();
+        console.log(dados.length)
+        setProdutos(dados);    
+    }
+
+    useEffect(() =>{
+        buscarDados();
+    }, []);
 
     return(
         <DashboardComponent id="outlet">
@@ -134,13 +243,23 @@ const Home = () =>{
                 </div>
 
                 <div id="containerBottom">
-                    <div>
+                    <div id="cabecalho">
                         <h4>Produtos</h4>
                         <Link id="addProduto" to="/productadd">+ Novo Produto</Link>
                     </div>
 
                     <div id="produtos">
-                        
+                        {produtos.map(produto => (
+                            <div className="produto" key={produto.id}>
+                                <img src={produto.imagem} alt={produto.nome} />
+                                <h1>{produto.nome}</h1>
+                                <p>R$ {produto.preco}</p>
+                                <div className="botoes">
+                                    <Link className="editar" to={`/productedit/${produto.id}`}>Editar</Link>
+                                    <Link className="remover" to="#">Excluir</Link>
+                                </div>
+                            </div>
+                        ))}
                     </div>
 
                     <div id="pageProdutoContainer">
